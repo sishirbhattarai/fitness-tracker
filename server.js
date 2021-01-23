05077b459cdd27a67a779d0175efe77b8406700d
Sishir <sishir.bhattarai@outlook.com>
2021-01-23 12:16:37 -0700

    added routes

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   });
   
   // routes
-  app.use(require("./routes"));
+  app.use(require("./routes/api.js"));
   
   app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
