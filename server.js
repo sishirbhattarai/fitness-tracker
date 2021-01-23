03ca2f335f716f650d946ff75c6ef6008cbd0d20
Sishir <sishir.bhattarai@outlook.com>
2021-01-22 17:05:48 -0700

    added models

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/tracker", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useFindAndModify: false
   });
   
   // routes
-  app.use(require("./routes/api.js"));
+  app.use(require("./routes"));
   
   app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
